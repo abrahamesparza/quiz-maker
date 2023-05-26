d5a8ca192f3f58fdf88633be0d4d6c58eab89f3d
Abraham Esparza <abrahamesparza.dev@gmail.com>
2023-05-25 22:27:12 -0700

    Add bare bones landing page

---
+++ client/src/Landing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Landing() {
+    return (
+        <div className="landing-page">
+            <h1>hi, you've reached the landing page.</h1>
+        </div>
+    )
+}
+
+export default Landing;
